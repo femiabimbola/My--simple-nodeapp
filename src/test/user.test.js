03238c0b7fe684244314c7993d19274fefbe7bad
Djtush <femiabimbola6@gmail.com>
2019-12-25 11:59:59 +0100

    added test

---
+++ src/test/user.test.js
@@ -0,0 +1,32 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../app";
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Post Route for api/v1/auth/signup", () => {
+  it("should create a new user", done => {
+    const user = {
+      email: "testforemail@gmail.com",
+      first_name: "Test",
+      last_name: "email",
+      password: "whateverhappens"
+    };
+    chai
+      .request(app)
+      .post("/api/v1/auth/signup")
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.data.should.have.property("firstName");
+        res.body.data.should.have.property("lastName");
+        res.body.data.should.have.property("password");
+        res.body.data.should.have.property("isAdmin");
+        done();
+      });
+  });
+
+  it();
+});
