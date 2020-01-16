ba8694d3f714fa7b392c794f189ef1d363072f21
unknown <femiabimbola6@gmail.com>
2020-01-16 15:41:38 +0100

    fixing travis

--- src/database/migrations.js
+++ src/database/migrations.js
@@ -3,39 +3,39 @@ import db from "./connection.db";
 import { dropTables, createTables } from "./queries";
 
 const {
-    dropUserTable, dropBusTable, dropTripTable,
-    dropBookingTable,
+  dropUserTable, dropBusTable, dropTripTable,
+  dropBookingTable,
 } = dropTables;
 
 const {
-    createUserTable, createBusTable, createTripTable, createBookingTable,
+  createUserTable, createBusTable, createTripTable, createBookingTable,
 } = createTables;
 
 const dropingTables = async () => {
-    try {
-        await db.query(dropUserTable);
-        await db.query(dropBusTable);
-        await db.query(dropTripTable);
-        await db.query(dropBookingTable);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await db.query(dropUserTable);
+    await db.query(dropBusTable);
+    await db.query(dropTripTable);
+    await db.query(dropBookingTable);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const creatingTables = async () => {
-    try {
-        await db.query(createUserTable);
-        await db.query(createBusTable);
-        await db.query(createTripTable);
-        await db.query(createBookingTable);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await db.query(createUserTable);
+    await db.query(createBusTable);
+    await db.query(createTripTable);
+    await db.query(createBookingTable);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export {
-    dropingTables,
-    creatingTables,
+  dropingTables,
+  creatingTables,
 };
 
 require("make-runnable");
